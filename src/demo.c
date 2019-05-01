70c70cf4c24d91d3035f2a1fb9feb5579653e98f
atmgnd <atmgnd@outlook.com>
2019-05-01 22:50:06 +0800

    modified

---
+++ src/demo.c
@@ -0,0 +1,24 @@
+#include "log.h"
+
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif
+
+
+int main()
+{
+#ifdef _MSC_VER
+	log_init("D:\\ttmp\\xx.log", 0);
+#else
+	log_init("./xx.log", 0);
+#endif
+
+	log_log(LOG_TRACE, "Hello %s", "world");
+	log_log(LOG_DEBUG, "Hello %s", "world");
+
+#ifdef _MSC_VER
+	system("pause");
+#endif
+
+	return 0;
+}
